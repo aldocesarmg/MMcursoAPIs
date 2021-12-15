0d37c425caeefd69dfdb8f4471fe421507924199
aldocesarmg <aldocesarmg@gmail.com>
2021-12-15 10:47:00 -0600

    Definicion del resto de endpoints; Archivo de explicacion anadido.

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello world')
 })
 
+//PARA LOS DEMAS ENPOINTS, LAS RUTAS Y LOS METODOS SE ESPECIFICAN DE LA SIGUIENTE MANERA:
+
+// Anadir pokemon
+app.post('/team/pokemons', () => {
+    res.status(200).send('Hello world!')
+})
+
+// Consultar equipo
+app.get('/team', () => {
+    res.status(200).send('Hello world!')
+})
+
+// Eliminar pokemon
+app.delete('/team/pokemons/:pokeid', () => {
+    res.status(200).send('Hello world!')
+})
+
+// Intercambiar el orden de pokemon
+app.put('/team', () => {
+    res.status(200).send('Hello world!')
+})
+
 // la aplicacion estara a la espera de cualquier peticion entrante con el codigo siguiente:
 app.listen(port, () => {
     console.log('Server started at port 3000.');
